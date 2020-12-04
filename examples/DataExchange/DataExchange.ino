260a61420eb516db0a0d6dd7262ee36bea119597
AndreyLabs <maslyany.andrew@yandex.ru>
2020-12-04 12:42:40 +0300

    changed sample

--- examples/DataExchange/DataExchange.ino
+++ examples/DataExchange/DataExchange.ino
@@ -1,20 +1,27 @@
 #include <IntlerCloudWifi.h>
+
+int luminosity = 32, temperature = 64;
+Cloud cloud("user", "pass", "myArduino");
+
 void printTemperature(double val) {
-  Serial.println("Print temperature " + String(val));
+  Serial.print("Print temperature " + String(val));
+  Serial.print(", time: ");
+  Serial.print(cloud.getHour());
+  Serial.print(" : ");
+  Serial.print(cloud.getMinute());
+  Serial.print(" : ");
+  Serial.println(cloud.getSecond());
 }
 
 void printLight(double val) {
   Serial.println("Print light " + String(val));
 }
 
-int luminosity = 32, temperature = 64;
-Cloud cloud("user", "pass", "myArduino");
 void setup() {
   Serial.begin(115200);
  
-  cloud.connect("ssid", "password");
-  cloud.setCurrentTime();
-  cloud.setInterval(8000);
+  cloud.connect("ssid", "password");  
+  cloud.setInterval(5000);
  
   cloud.addCommand("temperature", printTemperature);
   cloud.addCommand("light", printLight);
